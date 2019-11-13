a2ef48a28ee8f587ede2e8f68cacd9ab5b143fd5
rasgacode <rongybaba12@gmail.com>
2019-11-13 17:12:00 +0100

    car + jump

--- src/main/java/MoonBuggy.java
+++ src/main/java/MoonBuggy.java
@@ -7,12 +7,24 @@ import com.googlecode.lanterna.terminal.Terminal;
 import javax.swing.*;
 import java.util.*;
 import java.io.IOException;
+import java.lang.reflect.Method;
 
 public class MoonBuggy {
     public static void main(String[] args) throws IOException, InterruptedException {
+        String[][] originalPos = new String[][] {{"(", "69", "18"},{")", "71", "18"},
+                {"-", "72", "18"}, {"(", "73", "18"}, {")", "75", "18"},
+                {"O", "72", "17"}, {"m", "73", "17"}, {"m", "74", "17"}};
+
+        String[][] jumpOne = new String[][] {{"(", "69", "17"},{")", "71", "17"},
+                {"-", "72", "17"}, {"(", "73", "17"}, {")", "75", "17"},
+                {"O", "72", "16"}, {"m", "73", "16"}, {"m", "74", "16"}};
+
+        String[][] jumpTwo = new String[][] {{"(", "69", "16"},{")", "71", "16"},
+                {"-", "72", "16"}, {"(", "73", "16"}, {")", "75", "16"},
+                {"O", "72", "15"}, {"m", "73", "15"}, {"m", "74", "15"}};
+
         Terminal terminal = new DefaultTerminalFactory().createTerminal();
         Screen screen = new TerminalScreen(terminal);
-
 
         TextGraphics tg = screen.newTextGraphics();
         screen.startScreen();
@@ -32,45 +44,7 @@ public class MoonBuggy {
         }
     }
 
-
-
     private static void printSecondLayer(TextGraphics tg, Screen screen) throws IOException, InterruptedException {
-        /*Random r = new Random();
-        String[] ground = new String[1500];
-        int randomCounter = r.nextInt((17 - 7) + 1) + 7;
-
-        for (int i = 0; i < ground.length; i++) {
-            if (i == randomCounter) {
-                ground[i] = " ";
-                randomCounter += r.nextInt((17 - 7) + 1) + 7;
-            } else {
-                ground[i] = "#";
-            }
-        }
-        String[] map = new String[80];
-        for (int i = 0; i < map.length; i++) {
-            if (i == randomCounter && i > 20) {
-                map[i] = " ";
-                randomCounter += r.nextInt((17 - 7) + 1) + 7;
-            } else {
-                map[i] = "#";
-            }
-        }
-        int counter = 0;
-        while (counter < 10) {
-            for (int i = 0; i < ground.length - (map.length - 1); i++) {
-                for (int j = map.length - 1; j > 0; j--) {
-                    map[j] = map[j - 1];
-                }
-                map[0] = ground[i];
-
-                for (int k = 0; k < map.length; k++) {
-                    tg.putString(k, 19, map[k]);
-                }
-                Thread.sleep(100);
-            }
-            counter++;
-        }*/
         Random r = new Random();
         String[] ground = new String[1500];
         int randomCounter = r.nextInt((17 - 8) + 1) + 8;
@@ -108,4 +82,37 @@ public class MoonBuggy {
         }
 
     }
+
+    private static void carPos(TextGraphics tg, String[][] pos) throws InterruptedException {
+        for (String[] part: pos
+             ) {
+            tg.putString(Integer.parseInt(part[1]), Integer.parseInt(part[2]), part[0]);
+        }
+    }
+    private static void clear(TextGraphics tg) {
+        for (int i = 15; i < 19; i++) {
+            for (int j = 69; j < 76; j++) {
+                tg.putString(j, i, " ");
+            }
+        }
+    }
+    private static void jump(TextGraphics tg, Screen screen, String[][] oPos, String[][] JOPos, String[][] JTPos)
+            throws InterruptedException, IOException {
+        MoonBuggy.clear(tg);
+        MoonBuggy.carPos(tg, JOPos);
+        screen.refresh();
+        Thread.sleep(200);
+        MoonBuggy.clear(tg);
+        MoonBuggy.carPos(tg, JTPos);
+        screen.refresh();
+        Thread.sleep(400);
+        MoonBuggy.clear(tg);
+        MoonBuggy.carPos(tg, JOPos);
+        screen.refresh();
+        Thread.sleep(200);
+        MoonBuggy.clear(tg);
+        MoonBuggy.carPos(tg, oPos);
+        screen.refresh();
+        Thread.sleep(200);
+        }
 }
